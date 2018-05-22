617d29b3bf72d4be6340a1176d061b1f31af3fb2
GenBor <Generalovboris@outlook.com>
2018-05-22 09:04:59 +0300

    add requests_1

---  requests_1.sql
+++  requests_1.sql
@@ -1,7 +1,7 @@
 --select DISTINCT name,surname from django_view_artist 
 --where birth < '1990-01-01';
 --select name,date_released from django_view_song
---where name = "Meow";
+--where name LIKE("Meow%");
 --insert into django_view_song(author_id,date_released,name) VALUES(2,"2018-05-21","Voodoo dance");
 --UPDATE django_view_song
 --set author_id = 1
